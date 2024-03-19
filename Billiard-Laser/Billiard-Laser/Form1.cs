a4546a64d2379da00015bf17d4890a412c6f7e43
TikiTikiPlus <ro68@students.waikato.ac.nz>
2024-03-19 16:16:46 +1300

    Adding Billiard Laser folder, which contains forms apps

---
+++ Billiard-Laser/Billiard-Laser/Form1.cs
@@ -0,0 +1,10 @@
+namespace Billiard_Laser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ main.cs
@@ -0,0 +1,11 @@
+using System
+namespace Billiard_Laser
+{
+    class main
+    {
+        static voic Main(string[] args)
+        {
+            Console.WriteLine("Hello_World")
+        }
+    }
+}
