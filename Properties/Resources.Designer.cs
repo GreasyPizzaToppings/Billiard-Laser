88e327ce4e2c535f6e46604765b5c11f7d2be1b9
GreasyPizzaToppings <baxter.d.hills@gmail.com>
2024-06-03 20:07:37 +1200

    implement camera timeout check

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace billiard_laser.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace billiard_laser.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("billiard_laser.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace billiard_laser.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace billiard_laser.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap ballGrid2 {
+        public static System.Drawing.Bitmap ballGrid2 {
             get {
                 object obj = ResourceManager.GetObject("ballGrid2", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
@@ -73,7 +73,7 @@ namespace billiard_laser.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap birdEyeShot {
+        public static System.Drawing.Bitmap birdEyeShot {
             get {
                 object obj = ResourceManager.GetObject("birdEyeShot", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
