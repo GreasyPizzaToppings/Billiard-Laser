0e458c9d7d018101226fdc8f30a454492fea081d
GreasyPizzaToppings <baxter.d.hills@gmail.com>
2025-01-25 20:52:40 +1300

    Revert "increase timeout time to hopefully improve auto-connect to arduino"
    This reverts commit c27d0c8d19ecbf9a4942ec9ccddde70e8eba6406.

--- ArduinoController.cs
+++ ArduinoController.cs
@@ -78,7 +78,7 @@ public class ArduinoController : IDisposable
 
             serialPort.Open();
             
-            Thread.Sleep(timeout); // Allow Arduino to reset after connection
+            Thread.Sleep(500); // Allow Arduino to reset after connection
 
             // Send handshake request
             serialPort.WriteLine(HANDSHAKE_REQUEST);
