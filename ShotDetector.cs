3d484d3fcbe20ff04b936e37b9cd4f21ccff9351
GreasyPizzaToppings <baxter.d.hills@gmail.com>
2025-02-16 23:20:27 +1300

    refactor logic of shot detector + improve accuracy of detection. tested working on colours clearance

--- ShotDetector.cs
+++ ShotDetector.cs
@@ -1,9 +1,27 @@
-﻿public class ShotDetector
-{
-    private Shot currentShot;
-    private bool isCueBallMoving;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 
-    public event EventHandler<Shot> ShotFinished;
+public class ShotDetector : IDisposable
+{
+    private Shot? currentShot;
+    private bool isTrackingShot;
+    private bool disposed;
+
+    // Configurable parameters
+    public int MinFramesForShot = 20;             // Minimum frames needed to consider it a valid shot
+    public double MinTotalDistance = 50.0;        // Minimum total distance for a valid shot
+    public double StationaryThreshold = 5;       // Maximum movement allowed to consider ball stationary
+    public int StationaryFrameWindow = 15;        // Number of frames to check for ball being stationary
+
+    //keep a rolling window of this data even without a shot being tracked
+    private const int queueSize = 10;
+    private Queue<Point> recentPositions = new(queueSize);
+    private Queue<double> recentDisplacement = new(queueSize);
+    private Queue<VideoFrame> recentFrames = new(queueSize); 
+
+    public event EventHandler<Shot>? ShotFinished;
 
     public ShotDetector()
     {
@@ -12,80 +30,95 @@
 
     public void ResetState()
     {
-        // Dispose of any existing shot
-        if (currentShot != null)
-        {
-            currentShot.Dispose();
-        }
-
-        // Reset to initial state
-        isCueBallMoving = false;
+        if (currentShot != null) currentShot.Dispose();
         currentShot = new Shot();
+
+        isTrackingShot = false;
+        recentPositions.Clear();
+        recentDisplacement.Clear();
+
+        foreach (var frame in recentFrames) frame.Dispose();
+        recentFrames.Clear();
     }
 
     public void ProcessFrame(Ball cueBall, VideoFrame frame)
     {
-        //Console.WriteLine($"\nShot Detector: Frame {frame.Index}\n" +
-            //$"Cueball contour length: {cueBall.Contour.ToArray().Length}\n");
+        if (disposed) throw new ObjectDisposedException(nameof(ShotDetector));
+        if (!IsValidBall(cueBall)) return;
 
-        //invalid cue ball
-        if (cueBall == null || cueBall.Contour == null || cueBall.Contour.ToArray().Length <= 0) {
-            return;
-        }
+        // queues storing data to be ready for new shots
+        recentPositions.Enqueue(cueBall.Centre);
+        if (recentPositions.Count > queueSize) recentPositions.Dequeue();
 
-        currentShot.AddFrame(frame);
-        currentShot.cueBallPath.Add(cueBall.Centre);
+        recentDisplacement.Enqueue(cueBall.Displacement);
+        if (recentDisplacement.Count > queueSize) recentDisplacement.Dequeue();
 
-        Boolean ballNotMoving = false;
+        recentFrames.Enqueue(frame.Clone());
+        if (recentFrames.Count > queueSize) recentFrames.Dequeue().Dispose();
 
-        if (isCueBallMoving && currentShot != null && currentShot.FrameCount >= 25 && currentShot.FrameDistances.TakeLast(10).Sum() < 5) {
-            ballNotMoving = true;
-        }
+        currentShot?.AddFrame(frame.Clone());
+        currentShot?.cueBallPath.Add(cueBall.Centre);
+
+        if (StartedMoving() && !isTrackingShot) StartNewShot();
+        else if (isTrackingShot && StoppedMoving() && IsValidShot()) FinalizeShot();
+    }
+
+
+    private bool IsValidShot() {
+        if (currentShot == null || currentShot.DistanceTravelled < MinTotalDistance || currentShot.FrameCount < MinFramesForShot) return false;
+        return true;
+    }
+
+    private bool IsValidBall(Ball? cueBall)
+    {
+        if (cueBall == null || cueBall.Contour == null || cueBall.Contour.ToArray().Length <= 0) return false;
+        return true;
+    }
+
+    /// <summary>
+    /// The ball is moving if it moved more than the threshold since the last frame
+    /// </summary>
+    /// <returns></returns>
+    private bool StartedMoving()
+    {
+        return currentShot?.Displacement >= StationaryThreshold;
+    }
+
+    /// <summary>
+    /// The ball is deemed stopped if it hasnt moved above StationaryThreshold in the last StationaryFrameWindow frames
+    /// </summary>
+    /// <returns></returns>
+    private bool StoppedMoving() {
+        if (currentShot?.FrameDistances.TakeLast(StationaryFrameWindow).Sum() < StationaryThreshold) return true;
+        return false;
+    }
+
+    private void StartNewShot()
+    {
+        currentShot?.Dispose();
+        currentShot = new Shot();
+
+        // Add recent history
+        foreach (var position in recentPositions) currentShot.cueBallPath.Add(position);
+        foreach (var videoFrame in recentFrames) currentShot.AddFrame(videoFrame.Clone());
         
-        // criteria for ball moving:
-        // 1. ball has moved more than 10 pixel in the last frame
-        Boolean ballMoving = currentShot.Displacement > 10;
+        isTrackingShot = true;
+    }
 
-        //Console.WriteLine("Distance travelled in 1 frame: " + currentShot.Displacement);
-        //Console.WriteLine("Distance in Last 10: " + currentShot.FrameDistances.TakeLast(10).Sum());
-
-        if (ballMoving)
-        {
-            //mark as moving if not already
-            if (!isCueBallMoving)
-            {
-                isCueBallMoving = true;
-                currentShot.Dispose();
-                currentShot = new Shot();
-                currentShot.AddFrame(frame);
-                currentShot.cueBallPath.Add(cueBall.Centre);
-            }
-        }
-
-        else if (ballNotMoving)
-        {
-            // ball marked as not moving already
-            if (isCueBallMoving)
-            {
-                if (currentShot.DistanceTravelled > 10)
-                {
-                    OnShotFinished(currentShot);
-                }
-
-                // The shot is invalid (false shot), discard it
-                else
-                {
-                    //Console.WriteLine("False shot detected, discarding...");
-                }
-
-                currentShot = new Shot();
-                isCueBallMoving = false;
-            }
-        }
+    private void FinalizeShot()
+    {
+        OnShotFinished(currentShot);
+        ResetState();
     }
 
     protected virtual void OnShotFinished(Shot shot)
     {
-        ShotFinished?.Invoke(this, shot);
+        ShotFinished?.Invoke(this, shot.Clone());
+    }
+
+    public void Dispose()
+    {
+        if (currentShot != null) currentShot.Dispose();
+        disposed = true;
     }
 }
